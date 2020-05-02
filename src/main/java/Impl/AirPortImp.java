5c56907c6f4821389aa73f8abd22c9f67797c944
Cezary <chojnowski.czarek@gmail.com>
2020-05-02 12:45:23 +0200

    Creating interfaces and implementation method to findAirPortByCity, findAirPortByCountry, generateAIrPortsList

---
+++ src/main/java/Impl/AirPortImp.java
@@ -0,0 +1,31 @@
+package Impl;
+
+import dataAccess.IAirport;
+import model.Airport;
+
+import javax.jws.WebService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@WebService(endpointInterface = "dataAccess.IAirport")
+public class AirPortImp implements IAirport {
+    public List<Airport> findAirPortByCity(ArrayList<Airport> airports, String City) {
+        List<Airport> airportListByCity = airports.stream().filter(g -> g.city.equals(City)).collect(Collectors.toList());
+        return airportListByCity;
+    }
+
+    public List<Airport> findAirPortByCountry(ArrayList<Airport> airports, String Country) {
+        List<Airport> airportListByCountry = airports.stream().filter(g -> g.city.equals(Country)).collect(Collectors.toList());
+        return airportListByCountry;
+    }
+
+    public List<Airport> generateAirPortsList() {
+        List<Airport> airPortList = new ArrayList<>();
+        airPortList.add(new Airport(1,"Poland","Warsaw","Chopin","Franciszka Żwirki i Stanisława Wigury",1));
+        airPortList.add(new Airport(2,"Italy","Rome","Rome-Ciampino","Via Appia Nuova",1651));
+        airPortList.add(new Airport(3,"France","Paris","Paris-Roissy-Charles de Gaulle","Roissy-en-France",95700 ));
+        return airPortList;
+    }
+
+}
--- src/main/java/dataAccess/IAirport.java
+++ src/main/java/dataAccess/IAirport.java
@@ -3,10 +3,25 @@ package dataAccess;
 import model.Airport;
 import model.Flight;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import java.util.ArrayList;
 import java.util.List;
 
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL)
 public interface IAirport {
 
-    public List<Airport> findAirPortByCity(String City);
+    @WebMethod
+    public List<Airport> findAirPortByCity(ArrayList<Airport> airports, String City);
+
+    @WebMethod
+    public List<Airport> generateAirPortsList();
+
+    @WebMethod
+    public List<Airport> findAirPortByCountry(ArrayList<Airport> airports, String Country);
+
+
 
 }
